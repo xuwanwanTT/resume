1f1d54516c80baa4a76f3e2f688186454d9b435b
xuwanwan <1054749044@qq.com>
2017-10-26 01:37:56 +0800

    优化滚屏

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,25 +1,32 @@
 export default class Scroll{
   constructor(options){
     this.options = options
-    this.lock
+    this.lock = false
     this.pageIndex = 0
     this.bind()
   }
   bind(){
     window.addEventListener('mousewheel',(e)=>{
-      this.toScroll(e)
+      this.initScroll(e)
     })
   }
-  toScroll(e){
-    clearInterval(this.lock)
-    this.lock = setTimeout(()=>{
-      (e.wheelDelta < 0 && this.pageIndex <= 2 && this.pageIndex >= 0) ? (this.pageIndex++) : (this.pageIndex--)
-      if(this.pageIndex >= 2){
-        this.pageIndex = 2
-      }else if(this.pageIndex < 0){
-        this.pageIndex = 0
-      }
+  initScroll(e){
+    if(this.lock){return}
+    (e.wheelDelta < 0 && this.pageIndex <= 2 && this.pageIndex >= 0) ? (this.pageIndex++) : (this.pageIndex--)
+    if(this.pageIndex >= 2){
+      this.pageIndex = 2
+    }else if(this.pageIndex < 0){
+      this.pageIndex = 0
+    }
+    this.toScroll().then(()=>{
+      setTimeout(()=>{this.lock = false},800)
+    })
+  }
+  toScroll(){
+    this.lock = true
+    return new Promise((resolve,reject)=>{
       this.options.style.transform = `translateY(-${this.pageIndex}00%)`
-    },300)
+      resolve()
+    })
   }
 }
